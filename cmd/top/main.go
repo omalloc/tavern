c870c09fa228af79b78782a44e3b532522c05e83
Sendya <18x@loacg.com>
2026-02-04 14:21:28 +0800

    feat(metrics): add CPU and memory usage metrics to QsPlugin

---
+++ cmd/top/main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/dustin/go-humanize"
+	terminal "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+var (
+	endpoint     = ""
+	tickInterval = time.Second * 1
+	startAt      = time.Now()
+	uptime       = time.Unix(1767060001, 0)
+)
+
+func init() {
+	flag.StringVar(&endpoint, "endpoint", "http://localhost:8080/plugin/qs/graph", "The metrics endpoint to fetch data from tavern server.")
+	flag.DurationVar(&tickInterval, "interval", time.Second*1, "The interval to fetch metrics.")
+}
+
+func main() {
+	flag.Parse()
+
+	newDashboard()
+}
+
+func newDashboard() {
+	if err := terminal.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer terminal.Close()
+
+	collected := atomic.Bool{}
+	cpuPercent := atomic.Uint32{}
+	memUsage := atomic.Uint64{}
+	memTotal := atomic.Uint64{}
+	diskPercent := atomic.Uint64{} // mock
+
+	client := &http.Client{
+		Timeout:   time.Second,
+		Transport: &http.Transport{},
+	}
+
+	graph, _ := http.NewRequest(http.MethodGet, endpoint, nil)
+
+	fetch := func() map[string]float64 {
+		resp, err := client.Do(graph)
+		if err != nil {
+			collected.Store(false)
+			return nil
+		}
+
+		if resp != nil {
+			if resp.StatusCode != http.StatusOK {
+				if resp.Body != nil {
+					_ = resp.Body.Close()
+				}
+				collected.Store(false)
+				return nil
+			}
+		}
+
+		collected.Store(true)
+		var data map[string]float64
+		json.NewDecoder(resp.Body).Decode(&data)
+
+		cpuPercent.Store(uint32(data["cpu_percent"]))
+		memUsage.Store(uint64(data["mem_usage"]))
+		memTotal.Store(uint64(data["mem_total"]))
+		return data
+	}
+
+	termWidth, _ := terminal.TerminalDimensions()
+
+	// 基础监控指标 { qps, cpu, memory }
+	metricGrid := terminal.NewGrid()
+	metricGrid.SetRect(0, 3, termWidth, 20)
+
+	banner, bannerDraw := func() (*widgets.Paragraph, func()) {
+		banner := widgets.NewParagraph()
+		banner.SetRect(0, 0, termWidth, 3)
+		banner.Title = "Tavern"
+		banner.Border = true
+
+		textDraw := func() {
+			color := "fg:red"
+			status := "Disconnected"
+			if collected.Load() {
+				color = "fg:green"
+				status = "Connected"
+			}
+
+			banner.Text = fmt.Sprintf("%s | Sampling @ [%s](fg:blue) | [%s](%s) (%s) | Uptime %s",
+				endpoint, tickInterval.String(), status, color, startAt.Format(time.RFC1123), humanize.Time(uptime))
+		}
+		textDraw()
+
+		return banner, textDraw
+	}()
+
+	rater, raterDraw := func() (*widgets.Paragraph, func()) {
+		rater := widgets.NewParagraph()
+		rater.Title = "Requests"
+		rater.SetRect(0, 3, 50, 6)
+		rater.BorderStyle.Fg = terminal.ColorWhite
+		rater.TitleStyle.Fg = terminal.ColorCyan
+
+		draw := func() {
+			data := fetch()
+
+			rater.Text = fmt.Sprintf("\nRequests/sec: %d \nTotal: %d \n2xx : %d\n4xx : %d\n499 : %d\n5xx : %d",
+				int(data["total"]), int(data["total"]), int(data["2xx"]), int(data["4xx"]), int(data["499"]), int(data["5xx"]))
+		}
+
+		draw()
+		return rater, draw
+	}()
+
+	load, loadDraw := func() (*widgets.Gauge, func()) {
+		load := widgets.NewGauge()
+		load.Title = "CPU Usage"
+		load.Percent = int(cpuPercent.Load())
+		load.BarColor = terminal.ColorMagenta
+		load.BorderStyle.Fg = terminal.ColorWhite
+		load.TitleStyle.Fg = terminal.ColorCyan
+
+		return load, func() {
+			load.Percent = int(cpuPercent.Load()) + 10
+		}
+	}()
+
+	mem, memDraw := func() (*widgets.Gauge, func()) {
+		mem := widgets.NewGauge()
+		mem.Title = "Memory Usage"
+		usagePercent := 0
+		if memTotal.Load() > 0 {
+			usagePercent = int(float64(memUsage.Load()) / float64(memTotal.Load()) * 100)
+		}
+		mem.Percent = usagePercent
+		mem.BarColor = terminal.ColorGreen
+		mem.BorderStyle.Fg = terminal.ColorWhite
+		mem.TitleStyle.Fg = terminal.ColorCyan
+
+		return mem, func() {
+			usagePercent := 0
+			if memTotal.Load() > 0 {
+				usagePercent = int(float64(memUsage.Load()) / float64(memTotal.Load()) * 100)
+			}
+			mem.Percent = usagePercent
+			mem.Label = fmt.Sprintf("%d%% | Mem: %s / %s",
+				usagePercent,
+				humanize.Bytes(memUsage.Load()),
+				humanize.Bytes(memTotal.Load()),
+			)
+		}
+	}()
+
+	disk, diskDraw := func() (*widgets.Gauge, func()) {
+		disk := widgets.NewGauge()
+		disk.Title = "Disk Usage"
+		disk.Percent = int(diskPercent.Load()) + 70
+		disk.BarColor = terminal.ColorYellow
+		disk.BorderStyle.Fg = terminal.ColorWhite
+		disk.TitleStyle.Fg = terminal.ColorCyan
+
+		return disk, func() {
+			disk.Percent = int(diskPercent.Load()) + 70
+		}
+	}()
+
+	metricGrid.Set(
+		terminal.NewRow(1.0/2,
+			terminal.NewCol(1.0/2, rater),
+			terminal.NewCol(1.0/2,
+				terminal.NewRow(1.0/3, load),
+				terminal.NewRow(1.0/3, mem),
+				terminal.NewRow(1.0/3, disk),
+			),
+		),
+	)
+
+	// 高级监控指标 { 热点url 热点域名 热点磁盘 }
+	list := widgets.NewList()
+	list.Title = "Hot URLs"
+	list.SetRect(0, 12, termWidth, 30)
+	list.BorderStyle.Fg = terminal.ColorWhite
+	list.TitleStyle.Fg = terminal.ColorCyan
+	list.TextStyle.Fg = terminal.ColorYellow
+
+	terminal.Render(banner, metricGrid, list)
+
+	uiEvents := terminal.PollEvents()
+	ticker := time.NewTicker(time.Second).C
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			}
+
+			switch e.Type {
+			case terminal.ResizeEvent:
+				payload := e.Payload.(terminal.Resize)
+				termWidth = payload.Width
+				// termHeight = payload.Height
+
+				banner.SetRect(0, 0, termWidth, 3)
+				metricGrid.SetRect(0, 3, termWidth, 20)
+				list.SetRect(0, 12, termWidth, 30)
+
+				terminal.Clear()
+				terminal.Render(banner, metricGrid, list)
+			}
+
+		case <-ticker:
+			bannerDraw()
+			raterDraw()
+			memDraw()
+			diskDraw()
+			loadDraw()
+
+			terminal.Render(banner, metricGrid, list)
+		}
+	}
+}
